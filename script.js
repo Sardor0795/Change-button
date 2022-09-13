e0165e6cf9754bb60f9af4d4220e4bb985d90b6b
Sardor0795 <english.programmer.9595@gmail.com>
2022-09-13 07:59:36 +0500

    Fix bugs

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const button = document.querySelector("button");
+const button = document.querySelector(".btn");
 
 const width = document.querySelector(".width");
 const height = document.querySelector(".height");
